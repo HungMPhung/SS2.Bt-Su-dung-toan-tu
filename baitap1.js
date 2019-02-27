cbb7bc34e048731a643d906405b9a9de921653df
HungMPhung <minhhhunggtvt2011@gmail.com>
2019-02-27 11:25:34 +0700

    BaitapBienKieudulieuvaToantu

---
+++ baitap1.js
@@ -0,0 +1,10 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("inputWidth");
+inputHeight = prompt("InputHeight");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let are = width * height;
+document.write("The area is" +are);
